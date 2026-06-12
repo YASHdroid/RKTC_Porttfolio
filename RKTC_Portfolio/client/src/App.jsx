305615517073a58ded5acbac1c21b6f2f9541c6e
Shruti-369 <alewashruti@gmail.com>
2026-06-12 21:34:18 +0530

    done setup

--- RKTC_Portfolio/client/src/App.jsx
+++ RKTC_Portfolio/client/src/App.jsx
@@ -3,12 +3,11 @@ import About from './pages/about'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-400">RKTC Portfolio 🚀</h1>
+    </div>
   )
 }
+
 
 export default App
