3adfd77b69afdeba6c47ce8da5c5ff4c62c1f13b
Shruti-369 <alewashruti@gmail.com>
2026-06-13 23:18:57 +0530

    feat: add About page

--- RKTC_Portfolio/client/src/App.jsx
+++ RKTC_Portfolio/client/src/App.jsx
@@ -1,16 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import About from './pages/about'
+import Landing from './pages/Landing';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={
+        <Route path="/" element={<Landing />} />
+        <Route path="/arg" element={
           <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
             <h1 className="text-4xl font-bold text-blue-400">RKTC Portfolio 🚀</h1>
           </div>
         } />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   )
