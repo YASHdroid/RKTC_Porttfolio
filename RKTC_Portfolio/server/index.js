82f224cfc9f9a6b71d46bd20deb1745f9183ef6c
Yash Chopra <choprayash601@gmail.com>
2026-06-16 23:04:32 +0530

    Setup backend server and connect MongoDB

--- RKTC_Portfolio/server/index.js
+++ RKTC_Portfolio/server/index.js
@@ -1,12 +1,34 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
+dotenv.config();
+
+// Create backend application
 const app = express();
-//created en express application
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Database connection
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log("DB Error:", err);
+  });
+
+// Test route
 app.get("/", (req, res) => {
-    res.send("Backend is Running!")
-})
+  res.send("Backend Running");
+});
 
+// Start server
 app.listen(1122, () => {
-    console.log(`Server is running at the port ${1122}`);
-})
+  console.log("Server running on port 1122");
+});
