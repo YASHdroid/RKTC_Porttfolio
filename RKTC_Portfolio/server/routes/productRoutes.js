dd8c10c2f4146bcdb85b76c16a8677743aa6275b
Shruti-369 <alewashruti@gmail.com>
2026-06-27 11:31:50 +0530

    (.find and Delete) API

--- RKTC_Portfolio/server/routes/productRoutes.js
+++ RKTC_Portfolio/server/routes/productRoutes.js
@@ -64,3 +64,31 @@ router.get("/", async (req, res) => {
 
     }
 });
+
+//DELETE
+router.delete("/:id", async (req, res) => {
+    try {
+
+        const product = await Product.findByIdAndDelete(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({
+                success: false,
+                message: "Product not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Product deleted successfully"
+        });
+
+    } catch (err) {
+
+        res.status(500).json({
+            success: false,
+            message: "Server Error"
+        });
+
+    }
+});
