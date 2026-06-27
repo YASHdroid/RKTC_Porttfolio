99787ce6ea9290e56c8bd537f09246ce20d7b159
Shruti-369 <alewashruti@gmail.com>
2026-06-27 11:28:42 +0530

    add product(.create) API

---
+++ RKTC_Portfolio/server/routes/productRoutes.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.Router();
+
+const Product = require("../model/Product");
+
+//ADD PRODUCT
+router.post("/", async (req, res) => {
+    try {
+
+        const {
+            name,
+            description,
+            category,
+            image
+        } = req.body;
+
+        if (!name || !description || !category || !image) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            });
+        }
+
+        const product = await Product.create({
+            name,
+            description,
+            category,
+            image
+        });
+
+        res.status(201).json({
+            success: true,
+            message: "Product added successfully",
+            data: product
+        });
+
+    } catch (err) {
+
+        res.status(500).json({
+            success: false,
+            message: "Server Error"
+        });
+
+    }
+});
