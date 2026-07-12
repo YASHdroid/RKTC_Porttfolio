aa107707ae9641fb32f7b840c9faedad36b0f28f
Yash Chopra <choprayash601@gmail.com>
2026-07-12 13:18:27 +0530

    Replace Gmail SMTP with Resend API

--- RKTC_Portfolio/server/utils/sendEmail.js
+++ RKTC_Portfolio/server/utils/sendEmail.js
@@ -1,37 +1,26 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (inquiry) => {
-    const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
-    });
-
-    await transporter.sendMail({
-        from: process.env.EMAIL_USER,
-        to: process.env.ADMIN_EMAILS,
+    await resend.emails.send({
+        from: "RKTC <onboarding@resend.dev>",
+        to: process.env.ADMIN_EMAILS.split(","),
         replyTo: inquiry.email,
         subject: "New Inquiry Received - RKTC Portfolio",
-
         text: `
 A new inquiry has been submitted.
 
 Name: ${inquiry.name}
-
 Phone: ${inquiry.phone}
-
 Email: ${inquiry.email}
 
 Products:
-${inquiry.products.join(", ")}
+${inquiry.products?.join(", ") || "N/A"}
 
 Message:
 ${inquiry.message}
-`,
+        `,
     });
 };
 
